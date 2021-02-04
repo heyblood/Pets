9775c66f156d76ffd20cfb75d0da03588f71af60
blood <hey-blood@foxmail.com>
2021-02-05 05:30:19 +0800

    向 PetProvider 添加 UriMatcher

--- app/src/main/java/com/example/pets/data/PetProvider.java
+++ app/src/main/java/com/example/pets/data/PetProvider.java
@@ -2,15 +2,35 @@ package com.example.pets.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import java.net.URI;
+
 public class PetProvider extends ContentProvider {
 
     public static final String LOG_TAG = PetContract.class.getSimpleName();
+
+    /** URI pattern code to access whole table pets */
+    private static final int PETS = 100;
+
+    /** URI pattern code to access whole table pets to access a single row of the table pets */
+    private static final int PET_ID = 101;
+
+    // Creates a UriMatcher object.
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    static {
+        // to access whole table pets
+        sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS, PETS);
+
+        // to access a single row of the table pets
+        sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS+"/#", PET_ID);
+    }
 
     /**
      * Database helper object
