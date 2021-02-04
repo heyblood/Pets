c2dc164e959c5a7e1907fae5b5384cbf22bab928
blood <hey-blood@foxmail.com>
2021-02-05 03:24:53 +0800

    向 Contract 添加 URI

--- app/src/main/java/com/example/pets/data/PetContract.java
+++ app/src/main/java/com/example/pets/data/PetContract.java
@@ -1,5 +1,6 @@
 package com.example.pets.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 
@@ -14,10 +15,35 @@ public final class PetContract {
     }
 
     /**
+     * The "Content authority" is a name for the entire content provider, similar to the
+     * relationship between a domain name and its website.  A convenient string to use for the
+     * content authority is the package name for the app, which is guaranteed to be unique on the
+     * device.
+     */
+    public static final String CONTENT_AUTHORITY = "com.example.pets";
+
+    /**
+     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+     * the content provider.
+     */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    /**
+     * Possible path (appended to base content URI for possible URI's)
+     * For instance, content://com.example.android.pets/pets/ is a valid path for
+     * looking at pet data. content://com.example.android.pets/staff/ will fail,
+     * as the ContentProvider hasn't been given any information on what to do with "staff".
+     */
+    public static final String PATH_PETS = "pets";
+
+    /**
      * Inner class that defines constant values for the pets database table.
      * Each entry in the table represents a single pet.
      */
     public static final class PetEntry implements BaseColumns {
+
+        /** The content URI to access the pet data in the provider */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
 
         /**
          * Name of database table for pets
@@ -26,38 +52,38 @@ public final class PetContract {
 
         /**
          * Unique ID number for the pet (only for use in the database table).
-         * <p>
+         *
          * Type: INTEGER
          */
         public final static String _ID = BaseColumns._ID;
 
         /**
          * Name of the pet.
-         * <p>
+         *
          * Type: TEXT
          */
         public final static String COLUMN_PET_NAME = "name";
 
         /**
          * Breed of the pet.
-         * <p>
+         *
          * Type: TEXT
          */
         public final static String COLUMN_PET_BREED = "breed";
 
         /**
          * Gender of the pet.
-         * <p>
+         *
          * The only possible values are {@link #GENDER_UNKNOWN}, {@link #GENDER_MALE},
          * or {@link #GENDER_FEMALE}.
-         * <p>
+         *
          * Type: INTEGER
          */
         public final static String COLUMN_PET_GENDER = "gender";
 
         /**
          * Weight of the pet.
-         * <p>
+         *
          * Type: INTEGER
          */
         public final static String COLUMN_PET_WEIGHT = "weight";
